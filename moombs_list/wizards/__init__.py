5c252aec46d9b951da121667aa8c390f1952fb06
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-12-27 08:40:55 +0100

    moombs

--- moombs_list/wizards/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import baby_list_info_wizard
-from . import baby_list_complete_wizard
-from . import baby_list_cancel_wizard
-from . import baby_list_wallet_wizard
--- moombs_workflow_manager/services/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import action_logger
-from . import api_integration
-
